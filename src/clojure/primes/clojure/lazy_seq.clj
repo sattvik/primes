538e579e8b189f7b1e11520ec471efd79defde43
Daniel Solano Gómez <daniel@solanogomez.org>
2012-02-17 21:36:39 -0600

    Fix docstrings

--- src/clojure/primes/clojure/lazy_seq.clj
+++ src/clojure/primes/clojure/lazy_seq.clj
@@ -1,17 +1,20 @@
 (ns primes.clojure.lazy-seq
   "Generates primes using an infinite, lazy sieve.")
 
-(defn divides? [n d]
+(defn divides?
   "Returns true if n is divisible by d."
+  [n d]
   (zero? (rem n d)))
 
-(defn has-prime-factor? [n primes]
+(defn has-prime-factor?
   "Returns true if n has a factor in primes."
+  [n primes]
   (some #(divides? n %) primes))
 
-(defn next-prime [n known-primes]
+(defn next-prime
   "Given n and a list of prime factors, return the smallest number greater than
   or equal to n that is relatively prime to all of the numbers in known-primes."
+  [n known-primes]
   (first (remove #(has-prime-factor? % known-primes) (iterate inc n))))
 
 (def prime-seq
@@ -25,6 +28,6 @@
     (contains? prime-set n)))
 
 (defn get-primes
-  "Returns a list of all the prime numbers less than n"
+  "Returns a Primes for all primes less than n."
   [n]
   (LazySeqPrimes. (apply hash-set (take-while #(< % n) prime-seq))))
