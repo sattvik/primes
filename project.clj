14233332419d3ddc7784639c11e42765fb9aa468
Daniel Solano Gómez <daniel@solanogomez.org>
2012-03-03 17:02:12 -0600

    Update to latest Clojure

--- project.clj
+++ project.clj
@@ -5,5 +5,5 @@
   :java-source-path "src/java"
   :test-path "test/clojure"
   :warn-on-reflection true
-  :dependencies [[org.clojure/clojure "1.4.0-beta1"]]
+  :dependencies [[org.clojure/clojure "1.4.0-beta2"]]
   :dev-dependencies [[vimclojure/server "2.3.1"]])
